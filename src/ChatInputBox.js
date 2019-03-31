c7521c583e2c14ccacec45cc52163b3853579764
march213 <jane.molodetskaya@gmail.com>
2019-03-30 20:45:51 -0400

    feature: submit new messages to db

--- src/ChatInputBox.js
+++ src/ChatInputBox.js
@@ -1,16 +1,34 @@
 import React, { useState } from 'react'
+import { db } from './firebase'
 
 const ChatInputBox = () => {
   const [value, setValue] = useState('')
   return (
-    <div className="ChatInputBox">
+    <form
+      className="ChatInputBox"
+      onSubmit={async e => {
+        e.preventDefault()
+        // another way to get the input value
+        // const inputValue = e.target.elements[0].value
+        // e.target.reset() - after form submitting
+        await db
+          .collection('channels') // posible to query like so: channels/general/messages
+          .doc('general')
+          .collection('messages')
+          .add({
+            text: value,
+            createdAt: new Date(),
+          })
+        setValue('')
+      }}
+    >
       <input
         className="ChatInput"
         placeholder="Message #general"
         value={value}
         onChange={e => setValue(e.target.value)}
       />
-    </div>
+    </form>
   )
 }
 
