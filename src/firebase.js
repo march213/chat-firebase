43d1c01c2f925e3d05428f38681713ebaeaca3b2
march213 <jane.molodetskaya@gmail.com>
2019-04-03 15:52:31 -0400

    feature: sync realtime db with firestore for user presence

--- src/firebase.js
+++ src/firebase.js
@@ -23,19 +23,37 @@ export function setupPresence(user) {
     lastChanged: firebase.database.ServerValue.TIMESTAMP,
   }
 
-  // const isOnlineForFirestore = {}
-
   const isOnlineForRTDB = {
     state: 'online',
     lastChanged: firebase.database.ServerValue.TIMESTAMP,
   }
 
+  // firestore
+  const isOfflineForFirestore = {
+    state: 'offline',
+    lastChanged: firebase.firestore.FieldValue.serverTimestamp(),
+  }
+
+  const isOnlineForFirestore = {
+    state: 'online',
+    lastChanged: firebase.firestore.FieldValue.serverTimestamp(),
+  }
+
   const rtdbRef = rtdb.ref(`/status/${user.uid}`)
+  const userDoc = db.doc(`/users/${user.uid}`)
 
   rtdb.ref('.info/connected').on('value', async snapshot => {
-    if (snapshot.val() === false) return
+    if (snapshot.val() === false) {
+      userDoc.update({
+        status: isOfflineForFirestore,
+      })
+      return
+    }
     await rtdbRef.onDisconnect().set(isOfflineForRTDB)
     rtdbRef.set(isOnlineForRTDB)
+    userDoc.update({
+      status: isOnlineForFirestore,
+    })
   })
 }
 
